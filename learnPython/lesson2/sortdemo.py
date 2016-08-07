99abca5ae93f6e74ec483b6f4449f035757cee2b
unknown <zhangyajing@MININT-GTBEO3O.chinahr.com>
2016-08-07 16:13:42 +0800

    add lesson title

--- learnPython/lesson2/sortdemo.py
+++ learnPython/lesson2/sortdemo.py
@@ -7,6 +7,17 @@ contact:hebeizhangyajing@126.com
 @file: sortdemo.py
 @time:2016/7/31 21:56
 """
+"""
+ 1.自己实现一个排序算法，不能使用python内置的sorted和sort，具体哪种排序算法不限；
+函数接口：mysort(data)
+可选部分：【 对于有一定基础的同学，可以考虑扩展接口如下
+               mysort(data,key=somefunc,reveresed=True|False）
+                支持自定义比较函数，比如按照sin(x)或者abs(x)结果排序这样；
+                支持正序或者逆序排序；】
+2.实现测试用例；
+3.实现wordcount，自己找一篇英文文章或者句子，统计每个单词出现次数，并使用1中的排序算法输出排序后
+的结果；
+"""
 
 def mysort(array,key,reveresed):
     if key == 'abs':
